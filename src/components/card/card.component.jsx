d9fe390d915a2f7bce702aa52fecb7578c26eeb7
Azahar2 <a.zahar@baro-pfannenstein.de>
2021-05-22 21:11:09 +0200

    Changed the chars avatars

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,7 +5,7 @@ export const Card = (props) => (
   <div className="card-container">
     <img
       alt="monster"
-      src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+      src={`https://robohash.org/${props.monster.id}?set=set4&size=180x180`}
     ></img>
     <h2> {props.monster.name} </h2>
     <p>{props.monster.email}</p>
