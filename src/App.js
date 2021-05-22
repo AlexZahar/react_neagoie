73b9a595775a14188d48cf2e0ca692f34d85ff16
Azahar2 <a.zahar@baro-pfannenstein.de>
2021-05-22 20:49:30 +0200

    Created a searchbox componenent to filter the list

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@
 import "./App.css";
 import React, { Component } from "react";
 import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
@@ -18,9 +23,18 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
